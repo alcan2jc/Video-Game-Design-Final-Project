7ae93f6ab4e66686abba67b8088df6582f929795
alcan2jc <alcan2jc@github.com>
2021-11-12 21:23:00 -0500

    added documentation

--- rat.js
+++ rat.js
@@ -1,3 +1,4 @@
+//Collision function
 function hasCollided(x1, y1, x2, y2, width1, height1, width2, height2) {
     return (x1 + width1 > x2 &&
         x1 < x2 + width2 &&
@@ -5,14 +6,16 @@ function hasCollided(x1, y1, x2, y2, width1, height1, width2, height2) {
         y1 < y2 + height2);
 }
 
+// Rat enemy class. 
 class Rat {
     constructor(x, y, w, h) {
+        //Rat position and size
         this.x = x;
         this.y = y;
         this.width = w;
         this.height = h;
 
-        // constants
+        // constants for jumping and speed
         this.jumps = 0;
         this.max_jumps = 1;
         this.jmp_spd = -7.5;
@@ -21,12 +24,12 @@ class Rat {
         //moving variable. 0 = idle, 1 = left, 2 = right. 
         this.move = 2;
 
-        //game vars
-        this.dead = false;
-        this.hurt = false;
-        this.anim_counter_hurt = 0;
-        this.anim_counter_dead = 0;
-        this.lives = 5;
+        //game variables
+        this.dead = false; //death flag
+        this.hurt = false; //for when to animate being damaged
+        this.anim_counter_hurt = 0; //For which to animate in the hurt animations array
+        this.anim_counter_dead = 0; //For which to animate in the dead animations array
+        this.lives = 5; //amount of hits enemy can take. 
         //Invicibility frames after getting hit.
         this.invincibility = 0.5;
         this.invincibilityFrame = 0;
@@ -34,16 +37,16 @@ class Rat {
         this.vel = new p5.Vector(0, 0);
         this.acc = new p5.Vector(0, 0);
 
-        // 0: idle 1: running 2: airborn
-        this.animation_state = 0;
-        this.anim_counter = 0;
-        this.anim_counter_hurt = 0;
-        this.anim_counter_dead = 0;
-        this.anim_speed = 60 / 10; // frames per second
-        this.anim_speed_hurt = 60 / 2; // frames per second
-        this.anim_speed_dead = 60 / 10; // frames per second
+        this.animation_state = 0; // 0: idle 1: running 2: airborn
+        this.anim_counter = 0; //animation counter for moving
+        this.anim_counter_hurt = 0; //animation counter for hurt
+        this.anim_counter_dead = 0; //animation counter for dead
+        this.anim_speed = 60 / 10; // speed of running in frames per second
+        this.anim_speed_hurt = 60 / 2; // speed of hurt animations in frames per second
+        this.anim_speed_dead = 60 / 10; // speed of dead animations in frames per second
     }
 
+    //Checks rat to wall collision
     wallCollision() {
         let newx = this.x + this.vel.x
 
@@ -54,6 +57,7 @@ class Rat {
 
             let d = abs(this.x - block.x) + abs(this.y - block.y);
 
+            //only check collision if rat is close to that block
             if (d < 100) {
 
                 if (
@@ -76,6 +80,7 @@ class Rat {
 
             let d = abs(this.x - block.x) + abs(this.y - block.y);
 
+            //only check collision if rat is close to that block
             if (d < 100) {
                 if (
                     newx + this.width > block.x &&
@@ -93,9 +98,10 @@ class Rat {
                 }
             }
         }
-        return [newx, newy];
+        return [newx, newy]; //new = old for when there is collision
     }
 
+    //Returns the correct sprite for the animation state. 
     getSprite() {
         switch (this.animation_state) {
             // idle
@@ -139,22 +145,9 @@ class Rat {
         return null;
     }
 
+    //Check if Rat has been hit by the sword.
     checkSwordCollision() {
-        if (game.player.lastDir === 'left') {
-            if ((frameCount - this.invincibilityFrame) > this.invincibility * 60 && hasCollided(this.x, this.y, game.player.x - game.player.offsetX - 2.5 * game.player.width, game.player.y, this.width, this.height, game.player.swordWidth * 0.7, game.player.swordHeight / 5)) {
-                this.invincibilityFrame = frameCount;
-                this.lives--;
-            }
-        } else {
-            if ((frameCount - this.invincibilityFrame) > this.invincibility * 60 && hasCollided(this.x, this.y, game.player.x - game.player.offsetX, game.player.y, this.width, this.height, game.player.swordWidth * 0.7, game.player.swordHeight / 5)) {
-                this.invincibilityFrame = frameCount;
-                this.lives--;
-            }
-        }
-    }
-
-    checkSwordCollision() {
-        if (game.player.lastDir === 'left') {
+        if (game.player.lastDir === 'left') { //if player is facing left
             if ((frameCount - this.invincibilityFrame) > this.invincibility * 60 && hasCollided(this.x, this.y, game.player.x - game.player.offsetX - 2.5 * game.player.width, game.player.y, this.width, this.height, game.player.swordWidth * 0.7, game.player.swordHeight / 5)) {
                 this.invincibilityFrame = frameCount;
                 this.lives--;
@@ -165,7 +158,7 @@ class Rat {
                 }
 
             }
-        } else {
+        } else { //if player is facing right
             if ((frameCount - this.invincibilityFrame) > this.invincibility * 60 && hasCollided(this.x, this.y, game.player.x - game.player.offsetX, game.player.y, this.width, this.height, game.player.swordWidth * 0.7, game.player.swordHeight / 5)) {
                 this.invincibilityFrame = frameCount;
                 this.lives--;
@@ -178,6 +171,7 @@ class Rat {
         }
     }
 
+    //Updates rat movement, collision, lives, and animations. 
     update() {
         this.vel.x = 0;
         if (this.move === 1) { //1 = left
@@ -214,6 +208,7 @@ class Rat {
         this.y = newpos[1];
     }
 
+    //Draws the rat. 
     draw() {
         this.update();
         if (this.vel.x < 0) {
